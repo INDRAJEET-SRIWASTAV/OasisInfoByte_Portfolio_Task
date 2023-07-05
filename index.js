1cc241b194e926217cc718a8737a7721a67a049d
indrajeet sriwastav <121600838+INDRAJEET-SRIWASTAV@users.noreply.github.com>
2023-07-05 16:33:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed("#text",{
+    strings:["Frontend Developer","Programmer", "Web Developer", "Android Developer","Software Developer"],
+    typeSpeed :100,
+    backSpeed :100,
+    backDelay :1000,
+    loop : true
+});
